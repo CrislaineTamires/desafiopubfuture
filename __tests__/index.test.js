3262f0c803edae1c0a63ef7fcfa2a7c152545592
Crislaine <Crislainebbg@hotmail.com>
2022-01-16 21:08:41 -0300

    Adiciona testes

---
+++ __tests__/index.test.js
@@ -0,0 +1,26 @@
+// const contasRepository = require('../ContasRepository');
+// const despesasRepository = require('../DespesasRepository');
+// const receitasRepository = require('../ReceitasRepository');
+
+// jest.mock('../conta');
+// jest.mock('../despesa');
+// jest.mock('../receita');
+
+// test('Contas Read', async () => {})
+// test('Contas ReadBalance', async () => {})
+// test('Contas Transfer', async () => {})
+// test('Contas Add', async () => {})
+// test('Contas Update', async () => {})
+// test('Contas Delete', async () => {})
+
+// test('Despesas ReadAll', async () => {})
+// test('Despesas Read', async () => {})
+// test('Despesas Add', async () => {})
+// test('Despesas Update', async () => {})
+// test('Despesas Delete', async () => {})
+
+// test('Receitas ReadAll', async () => {})
+// test('Receitas Read', async () => {})
+// test('Receitas Add', async () => {})
+// test('Receitas Update', async () => {})
+// test('Receitas Delete', async () => {})
--- src/app.js
+++ src/app.js
@@ -2,10 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const app = express();
 
-//Informa que formato de arquivo vai ser recebido
 app.use(express.json());
-
-//Usa as rotas definidas no arquivo routes.js
 app.use(routes);
 
 module.exports = app;
